edcd72d8fb8669e5c3f99b10ecd59b62d10f3ed4
pineapplleboy <pineapplleboy@gmail.com>
2025-02-27 22:22:31 +0700

    create registration module

--- feature/registration/build.gradle.kts
+++ feature/registration/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.passtracker"
+    namespace = "com.example.passtracker.registration"
     compileSdk = 34
 
     defaultConfig {
@@ -45,6 +45,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(project(":feature:registration"))
 }
